28f68bd08cd2b5f188e89e46f5e38e7a6eb0bfb7
Pau Núñez <linkinpau.97@gmail.com>
2018-03-31 04:07:25 +0200

    Initial Commit

---
+++ Prac1/Main.java
@@ -0,0 +1,21 @@
+import ProbDes.*
+
+import aima.search.framework.Problem;
+import aima.search.framework.GraphSearch;
+import aima.search.framework.Search;
+import aima.search.framework.SearchAgent;
+import aima.search.informed.HillClimbingSearch;
+import aima.search.informed.SimulatedAnnealingSearch;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner
+
+public class Main {
+    
+    public static void main (String[] args) {
+    
+    }   
+}
