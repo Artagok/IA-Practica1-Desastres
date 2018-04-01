4a071016503a3a3424b5988efd587f3603fb0e37
adalmauWea <aleixdalmau@hotmail.es>
2018-04-01 16:57:40 +0200

    Create ProbDesSuccessorFunction.java

---
+++ Prac1/ProbDes/ProbDesSuccessorFunction.java
@@ -0,0 +1,15 @@
+package ProbDes;
+
+import aima.search.framework.Successor;
+import aima.search.framework.SuccessorFunction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProbDesSuccesorFunction implements SuccessorFunction {
+    public List getSuccessors(Object state){
+        ArrayList retval = new ArrayList();
+        
+        return retval;
+    }
+}
