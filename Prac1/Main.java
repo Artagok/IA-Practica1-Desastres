27b91ef6d78c302f483891f20f76eb0f01b92321
Melkor975 <37953386+Melkor975@users.noreply.github.com>
2018-04-03 20:52:05 +0200

    Update Main.java

--- Prac1/Main.java
+++ Prac1/Main.java
@@ -1,21 +1,65 @@
-import ProbDes.*
+// imporem totes les classes de ProbDes
+import ProbDes.*;
 
+//imports de aima
 import aima.search.framework.Problem;
 import aima.search.framework.GraphSearch;
 import aima.search.framework.Search;
 import aima.search.framework.SearchAgent;
 import aima.search.informed.HillClimbingSearch;
-import aima.search.informed.SimulatedAnnealingSearch;
+import aima.search.informed.SimulatedAnnealingSearch; //<--- aquest no fa falta de moment, ja però no passa res, és com un include q no es fa servir
 
+//imports de java
 import java.util.List;
 import java.util.Properties;
-import java.util.ArrayList;
+import java.util.ArrayList;         // d'aquests no se quins fem servir, pero sembla que no gaires, de moment deixa'ls, ja els traruem al final
 import java.util.Iterator;
-import java.util.Scanner
+import java.util.Scanner;
 
 public class Main {
     
-    public static void main (String[] args) {
+    public static void main(String[] args) throws Exception {
+        
+        int seed = 
+        int nGrups =
+        int nCentres = 
+        int nHeliXCentre = 
+        
+        ProbGaseEstat board = new ProbGasEstat (seed, nGrups , nCentres , nHeliXCentre);
+        
+        Problem p;
+        Search alg;
+        
+        /* Mirar aquestes creadores que no son ProbDesEstat (Montoya) */
+        p = new Problem (board, new ProbDesSuccesorFunction(), new ProbDesGoalTest(), new ProbDesHeuristicFunction());
+        alg = new HillClimbingSearch();
+        
+        SearchAgent agent = new SearchAgent(p, alg);
+        
+        printInstrumentation(agent.getInstrumentation());
+    }
     
-    }   
+    
+     private static void printInstrumentation(Properties properties) { //<----- Aquestes funcions ens les donen
+        Iterator keys = properties.keySet().iterator();
+        while (keys.hasNext()) {
+            String key = (String) keys.next();
+            String property = properties.getProperty(key);
+            System.out.println(key + " : " + property);
+        }
+    }
+
+    private static void printActions(List actions) { //<-----Aquestes funcions ens les donen | Aquest em sembla que es per SA
+        for (int i = 0; i < actions.size(); i++) {
+            String action = (String) actions.get(i);
+            System.out.println(action);
+        }
+    }
 }
+
+                                                    
+                                                    
+                                                    /* --- Experiment Especial 8 opcional --- */
+/* el tema de System.currentTimeMillis es per l'Experiment Especial 8 (opcional, 1 punt extra) --> pàg 6 del pdf, tenim aquesta setmana si el volem fer */
+
+
