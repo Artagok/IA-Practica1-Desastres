f8313c876be72c0018343746300b0186af102f8e
Pau Núñez <linkinpau.97@gmail.com>
2018-04-16 15:32:40 +0200

    Script per fer 10 execucions amb les 10 seeds que tenim

---
+++ Prac1/script.sh
@@ -0,0 +1,12 @@
+# 10 SEEDS = 32809..67121..89101..1234..1997..9997..05240..1872..37562..87523
+                                                # params: manual seed nGrups nCentres nHelixCentre HC/SA
+java -Xmx256m -cp Desastres.jar:AIMA.jar:ProbDes/*: Main 2 32809 100 5 1 1 >> output.txt
+java -Xmx256m -cp Desastres.jar:AIMA.jar:ProbDes/*: Main 2 67121 100 5 1 1 >> output.txt
+java -Xmx256m -cp Desastres.jar:AIMA.jar:ProbDes/*: Main 2 89101 100 5 1 1 >> output.txt
+java -Xmx256m -cp Desastres.jar:AIMA.jar:ProbDes/*: Main 2 1234 100 5 1 1 >> output.txt
+java -Xmx256m -cp Desastres.jar:AIMA.jar:ProbDes/*: Main 2 1997 100 5 1 1 >> output.txt
+java -Xmx256m -cp Desastres.jar:AIMA.jar:ProbDes/*: Main 2 9997 100 5 1 1 >> output.txt
+java -Xmx256m -cp Desastres.jar:AIMA.jar:ProbDes/*: Main 2 05240 100 5 1 1 >> output.txt
+java -Xmx256m -cp Desastres.jar:AIMA.jar:ProbDes/*: Main 2 1872 100 5 1 1 >> output.txt
+java -Xmx256m -cp Desastres.jar:AIMA.jar:ProbDes/*: Main 2 37562 100 5 1 1 >> output.txt
+java -Xmx256m -cp Desastres.jar:AIMA.jar:ProbDes/*: Main 2 87523 100 5 1 1 >> output.txt
